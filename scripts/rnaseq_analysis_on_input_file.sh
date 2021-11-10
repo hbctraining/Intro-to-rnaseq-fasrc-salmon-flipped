0d5457d96ee23d4efe7df2155055c0f3cc0888e0
Gammerdinger <wjgdinger@gmail.com>
2021-11-10 02:20:47 -0500

    Converted from O2 to FAS-RC
    Running fastqc with salmon installed is still producing a bug

--- scripts/rnaseq_analysis_on_input_file.sh
+++ scripts/rnaseq_analysis_on_input_file.sh
@@ -1,59 +1,67 @@
 #!/bin/bash/
-
 # This script takes a fastq file of RNA-seq data, runs FastQC, STAR, Qualimap and Salmon.
 # USAGE: sh rnaseq_analysis_on_input_file.sh <name of fastq file>
 
-# change directories to /n/scratch3/ so that all the analysis is stored there.
-cd /n/scratch3/users/r/$USER/rnaseq_hbc-workshop/
+# change directories to /n/holyscratch01/ so that all the analysis is stored there.
+
+cd /n/holyscratch01/PI_lab/$USER/rnaseq_hbc-workshop/
 
 # initialize a variable with an intuitive name to store the name of the input fastq file
+
 fq=$1
 
 # grab base of filename for naming outputs
+
 samplename=`basename $fq .subset.fq`
-echo "Sample name is $samplename"
+echo "Sample name is $samplename"   
 
 # specify the number of cores to use
+
 cores=6
 
-# directory with the genome and transcriptome index files + name of the gene annotation file
-genome=/n/groups/hbctraining/intro_rnaseq_hpc/reference_data_ensembl38/ensembl38_STAR_index
-transcriptome=/n/groups/hbctraining/rna-seq_2019_02/reference_data/salmon_index
-gtf=/n/groups/hbctraining/intro_rnaseq_hpc/reference_data_ensembl38/Homo_sapiens.GRCh38.92.1.gtf
+genome=/n/holylfs05/LABS/hsph_bioinfo/Everyone/Workshops/Intro_to_rnaseq/indicies/ensembl38_STAR_index
+transcriptome=/n/holylfs05/LABS/hsph_bioinfo/Everyone/Workshops/Intro_to_rnaseq/indicies/salmon_index
+gtf=/n/holylfs05/LABS/hsph_bioinfo/Everyone/Workshops/Intro_to_rnaseq/reference_data/Homo_sapiens.GRCh38.92.gtf
 
 # make all of the output directories
 # The -p option means mkdir will create the whole path if it 
 # does not exist and refrain from complaining if it does exist
+
 mkdir -p results/fastqc/
 mkdir -p results/STAR/
 mkdir -p results/qualimap/
 mkdir -p results/salmon/
 
 # set up output filenames and locations
+
 fastqc_out=results/fastqc/
-align_out=results/STAR/${samplename}
+align_out=results/STAR/${samplename}_
 align_out_bam=results/STAR/${samplename}_Aligned.sortedByCoord.out.bam
 qualimap_out=results/qualimap/${samplename}.qualimap
 salmon_out=results/salmon/${samplename}.salmon
 salmon_mappings=results/salmon/${samplename}_salmon.out
 
 # set up the software environment (use version numbers)
-module load fastqc/0.11.3
-module load gcc/6.2.0  
-module load star/2.7.0a
-module load samtools/1.3.1
-module load java/jdk-1.8u112
-module load qualimap/2.2.1
-module load salmon/1.4.0
+
+
+module load fastqc/0.11.8-fasrc01
+module load STAR/2.7.0e-fasrc01
+module load salmon/0.12.0-fasrc01
+
 unset DISPLAY
+
+export PATH=$PATH:/n/holylfs05/LABS/hsph_bioinfo/Everyone/holylfs/bcbio_nextgen/bin:/n/holylfs05/LABS/hsph_bioinfo/Everyone/holylfs/bcbio_nextgen/anaconda/bin
 
 echo "Processing file $fq"
 
 echo "Starting QC for $samplename"
 
-# Run FastQC and move output to the appropriate folder
-fastqc -o $fastqc_out $fq
+echo "Error comes here"
 
+# Run FastQC and move output to the appropriate folder
+fastqc -o $fastqc_out $fq 
+
+echo "Error stop here"
 
 # Run STAR
 STAR --runThreadN $cores --genomeDir $genome --readFilesIn $fq --outFileNamePrefix $align_out --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard
